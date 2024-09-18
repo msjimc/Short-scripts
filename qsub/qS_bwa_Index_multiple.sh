31133b862f22cbfb18b34196118febd82f6a46c1
Dr Ian M. Carr <msjimc@leeds.ac.uk>
2024-09-18 16:46:53 +0100

    Add files via upload

---
+++ qsub/qS_bwa_Index_multiple.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+## -t 1-n -v folder=<location of fasta files>  
+#############################################
+###Job submission config#####################
+#############################################
+# current environment and current working directory
+#$ -V -cwd
+# request time
+#$ -l h_rt=10:00:00
+# request memory per core
+#$ -l h_vmem=8G
+################################################
+####Job Details#################################
+################################################
+
+##set path for samtools and bwa
+export PATH=/home/home01/msjimc/marcRootApps/:$PATH
+
+
+
+##Check for the file of fasta file
+echo "Folder of files:"
+if [ -d "${folder}" ] ; then
+    echo "Good $folderis is a folder";
+elif [ -f "${folder}" ]; then
+    echo "Error ${folder} is a file";
+    exit 1
+else
+    echo "Error ${folder} is not valid";
+    exit 1
+fi
+
+##Get the read1 fastq.gz file and its pair
+fastaFile=$(ls $folder/*.fa | sed -n -e "$SGE_TASK_ID p")
+	echo FastA file: $fastaFile 
+
+echo Indexing reference file with BWA
+bwa index -a bwtsw $fastaFile 
+
+echo Indexing reference file with Samtools
+samtools faidx $fastaFile 
+
+
+
+echo done
---
+++ qsub/q_bwa_Index.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+## -v fasta=<location of BWA index>  
+#############################################
+###Job submission config#####################
+#############################################
+# current environment and current working directory
+#$ -V -cwd
+# request time
+#$ -l h_rt=10:00:00
+# request memory per core
+#$ -l h_vmem=8G
+################################################
+####Job Details#################################
+################################################
+
+##set path for samtools and bwa
+export PATH=/home/home01/msjimc/marcRootApps/:$PATH
+
+##Check for the file of fasta file
+echo "Folder of files:"
+if [ -d "${fasta}" ] ; then
+    echo "Error $fasta is is a folder";
+    exit 1
+elif [ -f "${fasta}" ]; then
+    echo "Good ${fasta} is a file";
+else
+    echo "Error ${fasta} is not valid";
+    exit 1
+fi
+
+echo Indexing reference file with BWA
+bwa index -a bwtsw $fasta
+
+echo Indexing reference file with Samtools
+samtools faidx $fasta
+
+echo Reporting
+jobID=$(qstat | egrep 'get_job_ID')
+job_ID=${jobID%" 0."*}
+echo $job_ID
+echo $(qstat -j $job_ID | egrep '^job-array tasks:')
+echo $(qstat -j $job_ID | egrep '^hard')
+echo $(qstat -j $job_ID | egrep '^submission')
+echo $(qstat -j $job_ID | egrep '^script_file')
+echo $(qstat -j $job_ID | egrep '^usage')
+
+echo done
